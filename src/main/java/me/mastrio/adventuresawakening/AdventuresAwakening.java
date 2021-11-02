0104a3f5a17f01eb41b89015bae60432f16601a9
MASTRIO <mastrio5772@gmail.com>
2021-11-02 19:42:18 +1000

    initialization setup
    (also the logger)

--- src/main/java/me/mastrio/adventuresawakening/AdventuresAwakening.java
+++ src/main/java/me/mastrio/adventuresawakening/AdventuresAwakening.java
@@ -1,7 +1,18 @@
 package me.mastrio.adventuresawakening;
 
-public class AdventuresAwakening {
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+public class AdventuresAwakening implements ModInitializer {
 
+    // Logger to log things to the console that contains thing that were logged to it using loggers such as this logger that is right here and is used for logging things into the logger
+    public static final Logger LOGGER = LogManager.getLogger("modid");
+
+    // Initialize the mod
+    @Override
+    public void onInitialize() {
+
+    }
 
 }
